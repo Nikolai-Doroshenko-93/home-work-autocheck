61f37e8eb91e4b0820c07e121eb7f5deceb7bebb
Nikolai-Doroshenko <doroshenko.nikolai.a@gmail.com>
2022-12-03 12:41:09 +0300

    HW8 completed

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let copyState=[...state]
+            action.payload === 'up'
+                ? copyState = [...state].sort((a, b)=>(a.age>(b.age)?1:-1))
+                : copyState = [...state].sort((a, b)=>(a.age>(b.age)?-1:1))
+            return copyState
 
-            return state // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            let copyState=[...state]
+            copyState = [...state].filter((a)=>(a.age>=18))
+            return copyState // need to fix
         }
         default:
             return state
