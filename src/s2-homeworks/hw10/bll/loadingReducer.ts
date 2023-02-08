d328f522738b0c576dcac2b84aad1b0e4730549c
Nikolai-Doroshenko <doroshenko.nikolai.a@gmail.com>
2023-02-08 19:03:59 +0300

    completed Hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -5,12 +5,17 @@ const initState = {
 export const loadingReducer = (state = initState, action: any): any => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
 
         default:
             return state
     }
 }
-
+export type StatePropsType = {
+    isLoading: boolean
+}
 type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
@@ -19,4 +24,4 @@ type LoadingActionType = {
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
-})
+}) as const
